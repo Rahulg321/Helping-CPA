08db4e286e445a5adb9f5696f104b3a7a2ff4998
Rahul Gupta <rg5353070@gmail.com>
2024-05-04 14:06:33 +0530

    added header and footer and homepage and resuable page component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { clsx } from "clsx";
+import { hind, montserrat } from "./fonts";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +19,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={clsx(montserrat.variable, hind.variable)}>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="8pP_lJBKWWCw5FpUWOGsVYea4jHoZ4bVNitNrYCI_EU"
+        />
+      </head>
+      <body className={clsx("min-h-screen bg-background antialiased")}>
+        <main className="parent-container">
+          <Header />
+          {children}
+          <Footer />
+        </main>
+      </body>
     </html>
   );
 }
